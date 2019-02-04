49037345c3188586a7f82931ba44c4aee0b4ba6e
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-02-04 22:34:38 +0000

    Add tests back.

--- src/tests/test_domain_models.py
+++ src/tests/test_domain_models.py
@@ -1,4 +1,4 @@
-# from unittest import mock
+from unittest import mock
 
 from src.domain_models.feeder import Feeder
 from src.typing_definitions.custom_types import TypeId
@@ -12,11 +12,11 @@ class TestFeeder:
     def test_init(self):
         feeder = Feeder(['A', 'B'], self.basic_feed_func, TypeId('feeder_id'))
         assert feeder.id == 'feeder_id'
-        # assert feeder.components == ['A', 'B']
-        # assert feeder.feed_func == self.basic_feed_func
+        assert feeder.components == ['A', 'B']
+        assert feeder.feed_func == self.basic_feed_func
 
-    # @mock.patch('uuid.uuid4')
-    # def test_auto_id(self, mock_uuid4):
-    #     mock_uuid4.return_value = 'feeder_id'
-    #     feeder = Feeder(['A', 'B'], self.basic_feed_func)
-    #     assert feeder.id == 'feeder_id'
+    @mock.patch('uuid.uuid4')
+    def test_auto_id(self, mock_uuid4):
+        mock_uuid4.return_value = 'feeder_id'
+        feeder = Feeder(['A', 'B'], self.basic_feed_func)
+        assert feeder.id == 'feeder_id'
