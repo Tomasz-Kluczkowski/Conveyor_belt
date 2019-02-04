f6f4a43122082b485b54179fbc03ddb5d3651120
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-02-04 22:45:28 +0000

    add repr to Feeder.
    Test if coverage now works.

--- src/domain_models/feeder.py
+++ src/domain_models/feeder.py
@@ -8,3 +8,6 @@ class Feeder:
         self.id = id if id else uuid.uuid4()
         self.components = components
         self.feed_func = feed_func
+
+    def __repr__(self):
+        return f'<Feeder(id={self.id}, components={self.components}, feed_func={self.feed_func.__name__})>'
