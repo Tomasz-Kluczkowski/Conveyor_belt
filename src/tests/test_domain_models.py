496be7f76121aaf5ce686c728ea269720856aa1f
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-02-04 22:57:14 +0000

    Test Feeder repr method.
    See if codecov works now.

--- src/tests/test_domain_models.py
+++ src/tests/test_domain_models.py
@@ -1,22 +1,35 @@
 from unittest import mock
 
+import pytest
+
 from src.domain_models.feeder import Feeder
 from src.typing_definitions.custom_types import TypeId
 
 
-class TestFeeder:
-    @staticmethod
-    def basic_feed_func():
-        return 1
+def basic_feed_func():
+    return 1
 
-    def test_init(self):
-        feeder = Feeder(['A', 'B'], self.basic_feed_func, TypeId('feeder_id'))
-        assert feeder.id == 'feeder_id'
-        assert feeder.components == ['A', 'B']
-        assert feeder.feed_func == self.basic_feed_func
+
+@pytest.fixture()
+def feeder_factory():
+    feeder = Feeder(['A', 'B'], basic_feed_func, TypeId('feeder_id'))
+    return feeder
+
+
+class TestFeeder:
+    def test_init(self, feeder_factory):
+        assert feeder_factory.id == 'feeder_id'
+        assert feeder_factory.components == ['A', 'B']
+        assert feeder_factory.feed_func == basic_feed_func
 
     @mock.patch('uuid.uuid4')
     def test_auto_id(self, mock_uuid4):
         mock_uuid4.return_value = 'feeder_id'
-        feeder = Feeder(['A', 'B'], self.basic_feed_func)
+        feeder = Feeder(['A', 'B'], basic_feed_func)
         assert feeder.id == 'feeder_id'
+
+    def test_feeder_repr_method(self, feeder_factory):
+        assert str(feeder_factory) == (
+            f"<Feeder(id=feeder_id, components=['A', 'B'], feed_func=basic_feed_func)>"
+        )
+
