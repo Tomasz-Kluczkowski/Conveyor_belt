8a49066c0e05fcf187eb7ecf16117433ad3fbe7e
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-08-09 14:57:42 +0100

    Change the interface of conveyor belt to encapsulate attributes better.
    Now the worker uses the public methods and the state is managed completely by the conveyor belt.

--- src/domain_models/conveyor_belt.py
+++ src/domain_models/conveyor_belt.py
@@ -18,6 +18,48 @@ class ConveyorBelt(Queue):
         self.__set_slot_states_to_free()
         self.__set_slots_to_empty()
 
+    @property
+    def slot_states(self):
+        return self.__slot_states
+
+    def check_item_at_slot(self, slot_number: int) -> Any:
+        """
+        Returns item at a conveyor belt slot.
+
+        Parameters
+        ----------
+        slot_number
+            Slot of the conveyor belt at which we need to check for item.
+        Returns
+        -------
+            Item at the slot_number.
+        """
+        # TODO: Make items private in the Queue. give necessary access methods if needed.
+        return self.items[slot_number]
+
+    def put_item_in_slot(self, slot_number: int, item: str):
+        self.__set_slot_state(slot_number=slot_number, state=ConveyorBeltState.BUSY)
+        self.items[slot_number] = item
+
+    def confirm_operation_finished(self, slot_number: int):
+        self.__set_slot_state(slot_number=slot_number, state=ConveyorBeltState.FREE)
+
+    def is_slot_busy(self, slot_number: int) -> bool:
+        return self.__get_slot_state(slot_number) == ConveyorBeltState.BUSY
+
+    def is_slot_empty(self, slot_number: int) -> bool:
+        return self.items[slot_number] == self.config.empty_code
+
+    def is_slot_free(self, slot_number: int) -> bool:
+        return self.__get_slot_state(slot_number) == ConveyorBeltState.FREE
+
+    def retrieve_item_from_slot(self, slot_number: int):
+        self.__set_slot_state(slot_number=slot_number, state=ConveyorBeltState.BUSY)
+        item = self.check_item_at_slot(slot_number=slot_number)
+        self.put_item_in_slot(slot_number=slot_number, item=self.config.empty_code)
+
+        return item
+
     def __set_slot_states_to_free(self):
         for slot_number in range(self.config.conveyor_belt_slots):
             self.__slot_states[slot_number] = ConveyorBeltState.FREE
@@ -26,66 +68,14 @@ class ConveyorBelt(Queue):
         for slot_number in range(self.config.conveyor_belt_slots):
             self.enqueue(self.config.empty_code)
 
-    def __check_validity_of_slot_number(self, slot_number: int):
-        if slot_number > self.config.conveyor_belt_slots - 1:
-            raise ValueError(INVALID_SLOT_NUMBER)
-
-    def check_at_slot(self, slot_number: int) -> Any:
-        """
-        Checks for item at a conveyor belt slot. If queue is too short we return empty just as you would examining a
-        real conveyor belt (without throwing an error).
-
-        Parameters
-        ----------
-        slot_number
-            Slot of the conveyor belt at which we need to check for item.
-        Returns
-        -------
-            Item at the slot_number if present or EMPTY config value. Note that empty can also be a valid value for
-            the slot set by the program.
-        """
-        self.__check_validity_of_slot_number(slot_number)
-        try:
-            return self.items[slot_number]
-        # We return empty as the queue is not yet filled by the feeder.
-        except IndexError:
-            return self.config.empty_code
-
-    @property
-    def slot_states(self):
-        return self.__slot_states
-
-    def set_slot_state(self, slot_number: int, state: str):
+    def __set_slot_state(self, slot_number: int, state: str):
         """
         Sets slot state at slot_number.
         """
-        self.__check_validity_of_slot_number(slot_number)
         self.__slot_states[slot_number] = state
 
-    def get_slot_state(self, slot_number: int) -> Union[str, None]:
+    def __get_slot_state(self, slot_number: int) -> Union[str, None]:
         """
         Returns slot state at slot_number or None if not valid slot accessed.
         """
         return self.__slot_states.get(slot_number)
-
-    def is_slot_busy(self, slot_number: int) -> bool:
-        return self.get_slot_state(slot_number) == ConveyorBeltState.BUSY
-
-    def is_slot_empty(self, slot_number: int) -> bool:
-        return self.items[slot_number] == self.config.empty_code
-
-    def is_slot_free(self, slot_number: int) -> bool:
-        return self.get_slot_state(slot_number) == ConveyorBeltState.FREE
-
-    def retrieve_item_from_slot(self, slot_number: int):
-        self.__check_validity_of_slot_number(slot_number)
-        self.set_slot_state(slot_number=slot_number, state=ConveyorBeltState.BUSY)
-        item = self.check_at_slot(slot_number=slot_number)
-        self.put_item_in_slot(slot_number=slot_number, item=self.config.empty_code)
-
-        return item
-
-    def put_item_in_slot(self, slot_number: int, item: str):
-        self.__check_validity_of_slot_number(slot_number)
-        self.set_slot_state(slot_number=slot_number, state=ConveyorBeltState.BUSY)
-        self.items[slot_number] = item
