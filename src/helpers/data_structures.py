731f492a3b1de19ead39d1b7ffa59b84133396af
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-08-09 15:32:13 +0100

    Change items to protected attribute.
    Add docstrings.

--- src/helpers/data_structures.py
+++ src/helpers/data_structures.py
@@ -1,21 +1,48 @@
+from typing import List, Any
+
+
 class Queue:
 
     def __init__(self):
-        self.items = []
+        self._items = []
 
     def enqueue(self, item):
-        self.items.insert(0, item)
+        """
+        Inserts item at the beginning of the queue.
+
+        Parameters
+        ----------
+        item
+            Item to insert.
+        """
+        self._items.insert(0, item)
 
     def dequeue(self):
-        return self.items.pop()
+        """
+        Returns last item in the queue.
+        """
+        return self._items.pop()
 
     @property
-    def is_empty(self):
-        return len(self.items) == 0
+    def is_empty(self) -> bool:
+        """
+        Returns true if queue is empty, false otherwise.
+        """
+        return len(self._items) == 0
 
     @property
-    def size(self):
-        return len(self.items)
+    def size(self) -> int:
+        """
+        Returns number of items in the queue.
+        """
+        return len(self._items)
+
+    @property
+    def items(self) -> List[Any]:
+        """
+        Returns contents of the queue as a list.
+        """
+        return self._items
 
 
 class MaxSizeQueue(Queue):
