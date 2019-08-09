b332db015c275e6f560543fb17bd5627d5919a5b
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-08-09 15:08:46 +0100

    Add docstrings to public methods of the conveyor belt.

--- src/domain_models/conveyor_belt.py
+++ src/domain_models/conveyor_belt.py
@@ -24,7 +24,7 @@ class ConveyorBelt(Queue):
 
     def check_item_at_slot(self, slot_number: int) -> Any:
         """
-        Returns item at a conveyor belt slot.
+        Returns item at a conveyor belt slot. The item remains in the slot. This is a peek operation.
 
         Parameters
         ----------
@@ -38,22 +38,79 @@ class ConveyorBelt(Queue):
         return self.items[slot_number]
 
     def put_item_in_slot(self, slot_number: int, item: str):
+        """
+        Inserts item at a given slot_number.
+
+        Parameters
+        ----------
+        slot_number
+            Slot of the conveyor belt at which we need to insert the item.
+        item
+            Item which we want to insert.
+        """
         self.__set_slot_state(slot_number=slot_number, state=ConveyorBeltState.BUSY)
         self.items[slot_number] = item
 
     def confirm_operation_finished(self, slot_number: int):
+        """
+        Confirms that the operation that was being carried out on slot slot_number has finished.
+        This sets the state of that slot back to free.
+
+        Parameters
+        ----------
+        slot_number
+            Slot of the conveyor belt at which we need to insert the item.
+        """
         self.__set_slot_state(slot_number=slot_number, state=ConveyorBeltState.FREE)
 
     def is_slot_busy(self, slot_number: int) -> bool:
+        """
+        Returns a boolean: True is slot is busy, false if free.
+
+        Parameters
+        ----------
+        slot_number
+            Slot of the conveyor belt at which we need to check if busy.
+        """
         return self.__get_slot_state(slot_number) == ConveyorBeltState.BUSY
 
     def is_slot_empty(self, slot_number: int) -> bool:
+        """
+        Returns a boolean: True is slot is empty, false if not.
+
+        Parameters
+        ----------
+        slot_number
+            Slot of the conveyor belt at which we need to check if empty.
+        """
+
         return self.items[slot_number] == self.config.empty_code
 
     def is_slot_free(self, slot_number: int) -> bool:
+        """
+        Returns a boolean: True is slot is free, false if busy.
+
+        Parameters
+        ----------
+        slot_number
+            Slot of the conveyor belt at which we need to check if free.
+        """
         return self.__get_slot_state(slot_number) == ConveyorBeltState.FREE
 
-    def retrieve_item_from_slot(self, slot_number: int):
+    def retrieve_item_from_slot(self, slot_number: int) -> Any:
+        """
+        Returns an item present in the slot: slot_number.
+
+        Parameters
+        ----------
+        slot_number
+            Slot of the conveyor belt from which we want to retrieve the item.
+
+        Returns
+        -------
+        item
+            Item in the slot with slot_number.
+        """
         self.__set_slot_state(slot_number=slot_number, state=ConveyorBeltState.BUSY)
         item = self.check_item_at_slot(slot_number=slot_number)
         self.put_item_in_slot(slot_number=slot_number, item=self.config.empty_code)
@@ -69,13 +126,7 @@ class ConveyorBelt(Queue):
             self.enqueue(self.config.empty_code)
 
     def __set_slot_state(self, slot_number: int, state: str):
-        """
-        Sets slot state at slot_number.
-        """
         self.__slot_states[slot_number] = state
 
     def __get_slot_state(self, slot_number: int) -> Union[str, None]:
-        """
-        Returns slot state at slot_number or None if not valid slot accessed.
-        """
         return self.__slot_states.get(slot_number)
