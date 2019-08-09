db88e0e962e2d202fab37f7b39b8a59fb6a0dd31
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-08-09 15:32:36 +0100

    Modify tests for Queue.

--- src/tests/test_data_structures.py
+++ src/tests/test_data_structures.py
@@ -4,6 +4,7 @@ from src.helpers.data_structures import Queue
 class TestQueue:
     def test_init(self):
         queue = Queue()
+        assert queue.size == 0
         assert queue.items == []
 
     def test_enqueue(self):
@@ -18,12 +19,18 @@ class TestQueue:
         queue.enqueue(2)
         queue.enqueue(3)
         assert queue.dequeue() == 1
-        assert queue.items == [3, 2]
+        assert queue.size == 2
 
-    def test_is_empty(self):
+    def test_is_empty_returns_true(self):
         queue = Queue()
         assert queue.is_empty
 
+        queue.enqueue(1)
+        queue.dequeue()
+        assert queue.is_empty
+
+    def test_is_empty_returns_false(self):
+        queue = Queue()
         queue.enqueue(1)
         assert not queue.is_empty
 
