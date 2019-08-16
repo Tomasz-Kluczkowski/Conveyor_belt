b7fd871519948a43c34bffeb1ef4ea7a383f2ce3
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-08-16 16:00:01 +0100

    Remove unused files.

--- src/domain_models/exceptions.py
+++
@@ -1,5 +0,0 @@
-class InvalidItemOperation(Exception):
-    def __init__(self, item, message=None):
-        if message is None:
-            message = f"Unable to pick up item: '{item}'."
-        super().__init__(message)
--- src/typing_definitions/custom_types.py
+++
@@ -1,3 +0,0 @@
-from typing import NewType
-
-TypeId = NewType('TypeId', str)
